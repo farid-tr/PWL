375d96f0441bc152deff17b4165add3d9dc34fdd
farid-tr <111202080014@mhs.dinus.ac.id>
2022-05-10 18:26:55 +0700

    8

---
+++ minggu xx/praktikum/session02.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+//pemeriksaan session
+if (isset($_SESSION['login'])) { //jika sudah login
+	//menampilkan isi session
+	echo "<h1>Selamat Datang ". $_SESSION['login'] ."</h1>";
+	echo "<h2>Halaman ini hanya bisa diakses jika Anda sudah login</h2>";
+	echo "<h2>Klik <a href='session03.php'>di sini (session03.php)</a> untuk LOGOUT</h2>";
+} else {
+	//session belum ada artinya belum login
+	die ("Anda belum login! Anda tidak berhak masuk ke halaman 	ini.Silahkan login <a href='session01.php'>di sini</a>");
+}
+?>
